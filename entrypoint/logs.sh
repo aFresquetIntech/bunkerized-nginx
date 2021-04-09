724a102e008ad63315ea6ce8c065f28484ce7570
aFresquetIntech <64793531+aFresquetIntech@users.noreply.github.com>
2021-04-09 17:22:32 +0200

    Adding REMOTE_SYSLOG environment variable

--- entrypoint/logs.sh
+++ entrypoint/logs.sh
@@ -19,3 +19,10 @@ touch /var/log/jobs.log
 replace_in_file "/etc/logrotate.conf" "%LOGROTATE_MAXAGE%" "$LOGROTATE_MAXAGE"
 replace_in_file "/etc/logrotate.conf" "%LOGROTATE_MINSIZE%" "$LOGROTATE_MINSIZE"
 echo "$LOGROTATE_CRON /opt/scripts/logrotate.sh > /dev/null 2>&1" >> /etc/crontabs/nginx
+
+# setup rsyslog
+if [ "$REMOTE_SYSLOG" != "" ] ; then
+    echo "local0.* @$REMOTE_SYSLOG;rawFormat" >> /etc/rsyslog.conf
+else
+    echo ""
+fi
